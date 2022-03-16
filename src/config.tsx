62e4ac8f0d463d76a8058015abf8dc5f7435e1a4
aziegenhirt <79174220+aziegenhirt@users.noreply.github.com>
2022-03-15 22:22:54 -0400

    Update config.tsx

--- src/config.tsx
+++ src/config.tsx
@@ -5,7 +5,7 @@ export const decimals = 2;
 export const denomination = 18;
 export const gasPrice = 1000000000;
 export const version = 1;
-export const gasLimit = 50000;
+export const gasLimit = 60000000;
 export const gasPerDataByte = 1500;
 
 export const walletConnectBridge = "https://bridge.walletconnect.org";
@@ -13,9 +13,9 @@ export const walletConnectDeepLink =
   "https://maiar.page.link/?apn=com.elrond.maiar.wallet&isi=1519405832&ibi=com.elrond.maiar.wallet.dev&link=https://maiar.com/";
 
 export const contractAddress =
-  "erd1qqqqqqqqqqqqqpgqcscrlpmpy0qm5n40aszjf6mnjthvla6rs5vqqfk69l";
+  "erd1qqqqqqqqqqqqqpgqu543yweynruv2kr3pz7ypm09yqxm42fh42hsxqs48j";
 export const bech32ContractAddress =
-  "00000000000000000500c4303f876123c1ba4eafec0524eb7392eecff7438518";
+  "00000000000000000500e52b123b2498f8c5587108bc40ede5200dbaa937aaaf";
 
 export const network: Dapp.NetworkType = {
   id: "mainnet",
