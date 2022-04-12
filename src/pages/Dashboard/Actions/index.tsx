de93068a17dac9c4a492a5d11361b226dea16a03
Draxon0012 <79174220+aziegenhirt@users.noreply.github.com>
2022-04-12 15:33:58 -0400

    Update index.tsx

--- src/pages/Dashboard/Actions/index.tsx
+++ src/pages/Dashboard/Actions/index.tsx
@@ -14,14 +14,14 @@ const Actions = () => {
   const [quantity, setQuantity] = React.useState(1);
   const [secondsLeft, setSecondsLeft] = React.useState(0);
 
-  const DROP_SIZE = 15;
-  const EGLD_PRICE = 1;
-  const LKMEX_PRICE = 1500000;
+  const DROP_SIZE = 10;
+  const EGLD_PRICE = 2;
+  const LKMEX_PRICE = 3100000;
 
   const getInfo = async () => {
     const url = `${network.apiAddress}/accounts/${contractAddress}/nfts/count`;
     const data = await fetch(url).then((res) => res.json());
-    isNaN(data) ? setNftsMinted(15) : setNftsMinted(15 - data);
+    isNaN(data) ? setNftsMinted(10) : setNftsMinted(10 - data);
   };
 
   const getTimeLeft = () => {
